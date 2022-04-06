21826c5fc3c2bf17aa76e8e55231fd51c0b0c241
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-06 06:54:24 +0200

    NX659J-UN: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: nubia Model: NX659J Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_NX659J-UN.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_NX659J-UN-user \
+    omni_NX659J-UN-userdebug \
+    omni_NX659J-UN-eng
